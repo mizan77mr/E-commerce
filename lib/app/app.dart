90ff3acaa254abcf7863c4409519a9b07b68d82c
Mizanur Rahman <mizanshaheb644@gmail.com>
2025-08-07 18:49:14 +0600

    fvm setup

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Fashion extends StatefulWidget {
+  const Fashion({super.key});
+
+  @override
+  State<Fashion> createState() => _FashionState();
+}
+
+class _FashionState extends State<Fashion> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:fashion/app/app.dart';
+import 'package:flutter/cupertino.dart';
+
+void main(){
+  runApp(const Fashion());
+}
